05b2eb10f85a23bc085b3ddae29895436020a330
hyun01115 <168633064+hyun01115@users.noreply.github.com>
2024-06-26 15:47:29 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,9 @@
+code = list(map(int, input().split()))
+ascending = [1,2,3,4,5,6,7,8]
+descending = [8,7,6,5,4,3,2,1]
+if code ==ascending:
+    print("ascending")
+elif code == descending:
+    print("descending")
+else:
+    print("mixed")
