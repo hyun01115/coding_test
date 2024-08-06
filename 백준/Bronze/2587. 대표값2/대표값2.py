f449716edfba2b4b89b2bdc123e671138bf9a764
hyun01115 <168633064+hyun01115@users.noreply.github.com>
2024-08-07 00:13:56 +0900

    [Bronze II] Title: 대표값2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -0,0 +1,18 @@
+a=[]
+
+for i in range(5):
+    b=int(input())
+    a.append(b)
+
+for i in range(5):
+    for j in range(len(a)-1-i):
+        if a[j]>a[j+1]:
+            temp=a[j]
+            a[j]=a[j+1]
+            a[j+1]=temp
+
+average= sum(a)//len(a)
+middle= a[len(a)//2]
+
+print(average)
+print(middle)
